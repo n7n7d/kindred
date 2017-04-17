9519efa1bf312af9f07087f5df6bf519b377766b
Nischay Namdev <nischay007@gmail.com>
2017-04-17 21:18:22 +0530

    Seller model added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |seller_params|
+      seller_params.permit(:name, :address, :phone_number, :company)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |seller_params|
+      seller_params.permit(:name, :address, :phone_number, :company)
+    end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :file
   # Show full error reports.
   config.consider_all_requests_local = true
 
